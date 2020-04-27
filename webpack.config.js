9d146f31d617c53a68db9a70f962b05e10c867bf
Admin <admin@Admins-MacBook-Pro.local>
2020-04-27 00:23:29 -0600

    still trying to debug the heroku version of this pwa

--- webpack.config.js
+++ webpack.config.js
@@ -62,10 +62,10 @@ module.exports = {
 
       icons: [
         {
-          src: "https://pwa-budget.herokuapp.com/icons/icon-512x512.png",
+          src: path.resolve(__dirname, "./public/icons/icon-512x512.png"),
           // the plugin will generate an image for each size
           // included in the size array
-          sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+          size: [192, 512],
         },
       ],
     }),
