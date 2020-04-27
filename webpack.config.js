4f339d0aab578f12d9c9d29e53da67a6bbe3a4b1
Admin <admin@Admins-MacBook-Pro.local>
2020-04-26 22:16:09 -0600

    worked on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const workbox = require("workbox-webpack-plugin");
 const html = require("html-webpack-plugin");
 const path = require("path");
 const css = require("mini-css-extract-plugin");
+const WebpackPwaManifest = require("webpack-manifest-plugin");
 
 module.exports = {
   entry: "./public/index.js",
@@ -40,6 +41,34 @@ module.exports = {
     ],
   },
   plugins: [
+    new WebpackPwaManifest({
+      // the name of the generated manifest file
+      filename: "manifest.json",
+
+      // we aren't using webpack to generate our html so we
+      // set inject to false
+      inject: false,
+
+      // set fingerprints to `false` to make the names of the generated
+      // files predictable making it easier to refer to them in our code
+      fingerprints: false,
+
+      name: "budget-app",
+      short_name: "budget",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
+      start_url: "https://pwa-budget.herokuapp.com/",
+      display: "standalone",
+
+      icons: [
+        {
+          src: path.resolve(__dirname, "/public/icons/icon-512x512.png"),
+          // the plugin will generate an image for each size
+          // included in the size array
+          size: [72, 96, 128, 144, 152, 192, 384, 512],
+        },
+      ],
+    }),
     new html({
       template: "./public/index.html",
       filename: "index.html",
